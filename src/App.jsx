83f597af0730263a54211e484fb808e918723bb5
Jithesh <jitheshjalapothu@gmail.com>
2025-10-03 21:46:21 -0700

    changed routing to expirence and education

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
         />
 
         {/* Experience route */}
-        <Route path="/expirence" element={<Expirence />} />
-        <Route path="/education" element={<Education />} />
+        <Route path="/jithesh-porto/expirence" element={<Expirence />} />
+        <Route path="/jithesh-porto/education" element={<Education />} />
       </Routes>
     </Router>
   );
